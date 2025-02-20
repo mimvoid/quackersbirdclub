d3c44c1ccd62b7cd7ece045b8a32ee6c566dce23
mimvoid <mimvoid@proton.me>
2025-02-20 09:23:12 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Provides tools for developing a Hugo site";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+  let
+    allSystems = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all;
+
+    toSystems = passPkgs: allSystems (system:
+      passPkgs (import nixpkgs { inherit system; })
+    );
+  in
+  {
+    devShells = toSystems (pkgs: {
+      default = pkgs.mkShell {
+        name = "Quackers Bird Club";
+
+        packages = with pkgs; [
+          hugo
+        ];
+      };
+    });
+  };
+}
