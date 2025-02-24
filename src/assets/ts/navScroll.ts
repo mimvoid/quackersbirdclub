cc7c86af40a07230427f040e1b8e3c9d58fb0b49
mimvoid <mimvoid@proton.me>
2025-02-24 00:19:47 -0500

    make nav bar respond to scrolling

---
+++ src/assets/ts/navScroll.ts
@@ -0,0 +1,17 @@
+// Show the nav bar when scrolling up,
+// and hide it when scrolling down
+
+const navBar = document.querySelector("header");
+
+if (navBar !== null) {
+  const navBarHeight = navBar.offsetHeight + 8; // offset to hide shadow more
+  let prevPos = window.scrollY;
+
+  window.onscroll = (() => {
+    if (document.body.scrollTop > navBarHeight || document.documentElement.scrollTop > navBarHeight) {
+      let currentPos = window.scrollY;
+      navBar.style.top = prevPos > currentPos ? "0" : `-${navBarHeight}px`;
+      prevPos = currentPos;
+    }
+  })
+}
