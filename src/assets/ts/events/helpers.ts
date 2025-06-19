231934a03392ebf6a8c4066d1407b7532dab9a9a
mimvoid <mimvoid@proton.me>
2025-06-19 12:56:25 -0400

    rewrite calendar event data fetcher

---
+++ src/assets/ts/events/helpers.ts
@@ -0,0 +1,97 @@
+import "add-to-calendar-button";
+
+class UserItem {
+  email: string;
+  self: boolean;
+}
+class DateItem {
+  dateTime: string;
+  timeZone: string;
+}
+export class EventItem {
+  kind: string;
+  etag: string;
+  id: string;
+  status: string;
+  htmlLink: string;
+  created: string;
+  updated: string;
+
+  summary: string;
+  description: string;
+  location: string;
+
+  creator: UserItem;
+  organizer: UserItem;
+
+  start: DateItem;
+  end: DateItem;
+
+  iCalUID: string;
+  sequence: number;
+  eventType: string;
+}
+
+export const month = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+export function mkElement(tag: string, setup: (self: HTMLElement) => void) {
+  const element = document.createElement(tag);
+  setup(element);
+  return element;
+}
+
+export function mkText(tag: string, textContent: string) {
+  const element = document.createElement(tag);
+  element.textContent = textContent;
+  return element;
+}
+
+export function amPmStr(dateTime: Date) {
+  const h = dateTime.getHours();
+  const m = dateTime.getMinutes();
+
+  if (m === 0) {
+    return h > 12 ? `${h - 12}pm` : `${h}am`;
+  }
+  return h > 12 ? `${h - 12}:${m}pm` : `${h}:${m}am`;
+}
+
+export function trimLoc(loc: string) {
+  // Remove ", USA" suffix
+  if (loc.endsWith(", USA")) {
+    return loc.substring(0, loc.length - 5);
+  }
+  return loc;
+}
+
+export function addCalendarButton(ev: EventItem) {
+  const addCal = document.createElement("add-to-calendar-button");
+
+  const start = new Date(ev.start.dateTime);
+  const end = new Date(ev.end.dateTime);
+
+  addCal.name = ev.summary;
+  addCal.description = ev.description;
+  addCal.options =
+    "'iCal','Google','Outlook.com','Microsoft365','MicrosoftTeams','Apple','Yahoo'";
+  addCal.location = ev.location;
+  addCal.startDate = start.toDateString();
+  addCal.endDate = end.toDateString();
+  addCal.startTime = start.toTimeString();
+  addCal.endTime = end.toTimeString();
+
+  return addCal;
+}
---
+++ src/assets/ts/events/iCalEvents.ts
@@ -0,0 +1,99 @@
+import process from "node:process"
+import {
+  EventItem,
+  mkElement,
+  mkText,
+  month,
+  amPmStr,
+  trimLoc,
+  addCalendarButton,
+} from "./helpers.ts";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  // The URL to access the Quackers Bird Club calendar JSON data
+  const urlBase =
+    "https://www.googleapis.com/calendar/v3/calendars/quackersbirdclub@gmail.com/events";
+
+  process.loadEnvFile();
+  const params = new URLSearchParams({
+    key: process.env.CALENDAR_KEY,
+  });
+
+  const res = await fetch(urlBase + "?" + params);
+  if (!res.ok) {
+    throw new Error(
+      `Request to the Quackers Bird Club Google Calendar failed: ` + res,
+    );
+  }
+
+  const data = await res.json().catch(console.error);
+
+  const tablerClock = `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0-18 0"/><path d="M12 7v5l3 3"/></g></svg>`;
+
+  const tablerMapPin = `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0-6 0"/><path d="M17.657 16.657L13.414 20.9a2 2 0 0 1-2.827 0l-4.244-4.243a8 8 0 1 1 11.314 0"/></g></svg>`;
+
+  const eventNode = document.getElementById("events")!;
+  const timelines = new Map<number, HTMLElement>();
+
+  for (const rawEvent of data.items) {
+    const ev: EventItem = Object.assign(new EventItem(), rawEvent);
+
+    const start = new Date(ev.start.dateTime);
+    const end = new Date(ev.end.dateTime);
+
+    // This is an event, add it to the timeline
+    const year = start.getFullYear();
+    let timeline = timelines.get(year);
+
+    if (timeline == undefined) {
+      // Set up a new year timeine
+      const yearDiv = eventNode.appendChild(document.createElement("div"));
+      yearDiv.appendChild(mkText("h2", String(year)));
+
+      timeline = yearDiv.appendChild(
+        mkElement("ul", (self) => {
+          self.classList.add("timeline");
+          self.id = `timeline-${year}`;
+        }),
+      );
+
+      // Save it in the dictionary
+      timelines.set(year, timeline);
+    }
+
+    const card = timeline.appendChild(
+      mkElement("li", (self) => self.classList.add("card")),
+    );
+
+    // Create date section
+    const dateWrapper = card.appendChild(document.createElement("div"));
+    const date = dateWrapper.appendChild(document.createElement("time"));
+    date.dateTime = start.toISOString();
+    date.innerHTML = `${month[start.getMonth()]} <span class="day">${start.getDate()}</span>`;
+
+    const text = card.appendChild(
+      mkElement("div", (self) => self.classList.add("timeline-text")),
+    );
+
+    // Summary
+    text.appendChild(mkText("h3", ev.summary));
+
+    // Time
+    const time = text.appendChild(document.createElement("p"));
+    time.innerHTML += tablerClock;
+    time.append(amPmStr(start) + "–" + amPmStr(end));
+
+    if (ev.location) {
+      const loc = text.appendChild(document.createElement("p"));
+      loc.innerHTML += tablerMapPin;
+      loc.append(trimLoc(ev.location));
+    }
+
+    if (ev.description) {
+      text.appendChild(mkText("p", ev.description));
+    }
+
+    // FIX: The add to calendar button does not actually work
+    text.appendChild(addCalendarButton(ev));
+  }
+});
